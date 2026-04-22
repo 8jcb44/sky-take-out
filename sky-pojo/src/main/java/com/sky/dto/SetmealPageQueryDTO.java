3d251bebd417319aec3043418babee3508f32566
xaiomei <hello@xiaomei.cn>
2026-04-22 14:37:07 +0800

    套餐分页查询功能业务代码开发

--- sky-pojo/src/main/java/com/sky/dto/SetmealPageQueryDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/SetmealPageQueryDTO.java
@@ -7,10 +7,13 @@ import java.io.Serializable;
 @Data
 public class SetmealPageQueryDTO implements Serializable {
 
+    //当前页码
     private int page;
 
+    //每页显示数量
     private int pageSize;
 
+    //名称查询参数
     private String name;
 
     //分类id
