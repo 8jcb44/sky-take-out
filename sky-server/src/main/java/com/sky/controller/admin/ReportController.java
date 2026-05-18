3fc299fe77ed719ae351d9b8578f51204635c795
xaiomei <hello@xiaomei.cn>
2026-05-18 17:36:21 +0800

    用户数据统计功能代码开发

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -3,7 +3,9 @@ package com.sky.controller.admin;
 import com.sky.result.Result;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import io.swagger.annotations.ApiOperation;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -40,6 +42,22 @@ public class ReportController {
             LocalDate end){
         log.info("营业额数据统计: {},{}",begin,end);
         return Result.success(reportService.getTurnoverStatistics(begin,end));
+    }
 
+    /**
+     * 用户统计
+     * @param begin
+     * @param end
+     * @return
+     */
+    @GetMapping("/userStatistics")
+    @ApiOperation("用户统计")
+    public Result<UserReportVO> userStatics(
+            @DateTimeFormat(pattern = "yyyy-MM-dd")
+            LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd")
+            LocalDate end){
+        log.info("用户数据统计: {},{}",begin,end);
+        return Result.success(reportService.getUsersStatistics(begin,end));
     }
 }
